3147c7f82c1ef938f93842312f29ebbfac5ff073
VITYA SHARNENKOV <72027510+sharixxx@users.noreply.github.com>
2020-12-10 20:45:55 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,127 @@
+var swiper = new Swiper('.swiper-container', {
+  //nawigacja
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  pagination:{
+    // bullets
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable:true,
+    dynamicBullets:true,
+  },
+  //progress bar
+  type:'progressbar',
+  // scroll
+  // scrollbar:{
+  //   el: '.swiper-scrollbar',
+  //   draggable:true
+  // },
+  //efekt
+  effect: 'coverflow',
+  //kursor myszy na sliderze
+  grabCursor: true,
+  //na PC
+  // simulateTouch:true,
+  //aktywny slide w srodku
+  centeredSlides: true,
+  //paralax efekt
+  // parallax:true,
+  //odstep miedzy slidem
+  spaceBetween:30,
+ //ilosc slidow zmienionych odrazu w grupe
+//  slidesPerGroup:'1',
+ //ktory slide poczatkowy
+//  initialSlide:1,
+//czestotliwosc zmiany
+  touchRatio:2,
+  //swobodnie mozemy zmieniac
+  // freeMode: true,
+  //kat swipe
+  touchAngle: 45,
+  //predkosc w ms
+  speed:500,
+  //wertykalny slider
+  // direction:'vertical',
+  //auto-zmienianie
+  // autoplay:{
+    //pauza
+    // delay:2000,
+    //skonczyc na ostatnim slidzie
+    // stopOnLastSlide:true,
+    //wylaczyc przy recznym zmienianiu
+    // disableOnInteraction:true,
+  // },
+  //adaptywnosc
+  // breakpoints: {
+  //   320:{
+  //     slidesPerView:1,
+  //   },
+  //   480:{
+  //     slidesPerView:2,
+  //   },
+  //   992:{
+  //     slidesPerView:3,
+  //   }
+  // },
+
+  //nie pobiera odrazu zdjec,szybsza witryna
+  // preloadImages:false,
+  // lazy:{
+  //   loadOnTransitionStart:false,
+  //   loadPrevNext:false,
+  // },
+  // //podgladac za slidami
+  // watchSlidesProgress:true,
+  // //dodanie klasy widocznym slidam
+  // watchSlidesVisibility:true,
+  // //nawigacja w hash
+  // hashNavigation:{
+  //   watchState:true,
+  // },
+  //zoom zdjec
+  zoom:{
+    maxRatio:5,
+    minRatio:1,
+  },
+//klawiatura
+  keyboard: {
+    //on/off
+    enabled: true,
+    //tylko kiedy ogladasz dziala klawiatura
+    onlyInViewport:true,
+    //klawisze Pageup , pagedown
+    pageUpDown:true,
+  },
+
+  //mysza
+  mousewheel:{
+    // czestotliwosc
+    sensitivity:1,
+    //klasa obiekta na ktorym bedzie dzialala mysza
+    //eventsTarget: "swiper-container",
+  },
+  //auto-wysokosc pod wszystko
+  autoHeight:true,
+  //ilosc fotografii na sliderze
+  slidesPerView:'auto',
+  //zmienia obraz na klik
+  // slideToClickedSlide:true,
+  //wylacza slider kiedy 1 obrazek
+  watchOverflow:true,
+  //efekt
+  coverflowEffect: {
+    rotate: 10,
+    stretch: 0,
+    depth: 400,
+    modifier: 1,
+    slideShadows: true,
+  },
+  //caly czas nie ma konca: loop
+  loop:true
+});
+
+
+
+
---
+++ main.js
@@ -0,0 +1,33 @@
+    
+$(window).on("scroll resize", function() {
+    if ($(this).scrollTop() > 1){
+    $('header').addClass("fixed");
+    }
+    else{
+    $('header').removeClass("fixed");
+    }
+    });
+
+
+    function checkScroll(scrollPos, introH) {
+        if( scrollPos > introH ) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+/* Smooth scroll */
+
+$("[data-scroll]").on("click", function(event) {
+    event.preventDefault();
+
+    let elementId = $(this).data('scroll');
+    let elementOffset = $(elementId).offset().top;
+    console.log(elementOffset);
+    // nav.removeClass("show");
+
+    $("html, body").animate({
+        scrollTop: elementOffset -50
+    }, 800);
+});

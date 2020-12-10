74245755e02ac7d71e31db5d73171aa36b94f184
VITYA SHARNENKOV <72027510+sharixxx@users.noreply.github.com>
2020-12-10 21:31:11 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -31,3 +31,25 @@ $("[data-scroll]").on("click", function(event) {
         scrollTop: elementOffset -50
     }, 800);
 });
+
+$(function() {
+    // при нажатии на кнопку scrollup
+    $('.scrollup').click(function() {
+      // переместиться в верхнюю часть страницы
+      $("html, body").animate({
+        scrollTop:0
+      },800);
+    })
+  })
+  // при прокрутке окна (window)
+  $(window).scroll(function() {
+    // если пользователь прокрутил страницу более чем на 200px
+    if ($(this).scrollTop()>200) {
+      // то сделать кнопку scrollup видимой
+      $('.scrollup').fadeIn();
+    }
+    // иначе скрыть кнопку scrollup
+    else {
+      $('.scrollup').fadeOut();
+    }
+  });
